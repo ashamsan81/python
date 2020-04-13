d28ee1472332a9bd85d205595b5d6285a4e726a2
ashamsan81 <a.shamsan81@gmail.com>
2020-04-13 23:29:36 +1200

    escape char

---
+++ escape_char.py
@@ -0,0 +1,13 @@
+#------------------------------
+#Escape Sequences Characters
+#\b => backspace
+#\newline => Escape New Line + \
+#
+#
+
+
+
+print("Hello\bWorld")
+print("Hello\
+    Love\
+    Python")
