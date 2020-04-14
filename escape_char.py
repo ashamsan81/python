ce7f2d2b7c3563805bd86266934fc5910ae02a8c
ashamsan81 <a.shamsan81@gmail.com>
2020-04-14 17:44:54 +1200

    Escape Chars

--- escape_char.py
+++ escape_char.py
@@ -2,12 +2,23 @@
 #Escape Sequences Characters
 #\b => backspace
 #\newline => Escape New Line + \
-#
-#
-
-
+#\ => to escape special charcter
+#\n => newline
+#\r => Carriage Return
+#\t => Horizintal Tab
+#\xhh => Character Hex Value you replace hh with the hexadecimal value
 
 print("Hello\bWorld")
 print("Hello\
     Love\
     Python")
+
+print("ahmed.shamsan@gmail.com\"myemail\"")
+print("I love back Slash \\")
+print("first line\nsecond line")
+
+print("123456789\rreplace\nsee result is only the word replace and remaining numbers 89")
+
+print("Love\tPython")
+
+print("\x4F")
