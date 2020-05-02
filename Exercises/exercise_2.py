9d36d11eb2f24735e2900d3aba589436eab03016
ashamsan81 <a.shamsan81@gmail.com>
2020-05-03 02:23:39 +1200

    enhacing exercise 2

--- Exercises/exercise_2.py
+++ Exercises/exercise_2.py
@@ -1,12 +1,25 @@
 ### exercise 2
 myPrimLeagTuple = "Liverpool", "Man City", "Leicester", "Chelsea", "Man Utd", "Wolves", "Sheffield Utd", "Spurs", "Arsenal", "Burnley", "Crystal Palace", "Everton", "Newcastle", "Southampton", "Brighton", "West Ham", "Watford", "Bournemouth", "Aston Villa", "Norwich"
-print(len(myPrimLeagTuple))
+a = 0
+myPrimLeagOrderedTuple = sorted(myPrimLeagTuple)
+print("Prime League Teams in Alphapet Order")
+while a < len(myPrimLeagOrderedTuple):
+    print(f"{a + 1} - {myPrimLeagOrderedTuple[a]}")
+    a += 1
+
 print("=" * 40)
-print("The Top 5 are: {}".format(myPrimLeagTuple[0:5]))
-print("=" * 40)
-print("The Last 4 are: {},{}".format(myPrimLeagTuple[-1],myPrimLeagTuple[-4:]))
-print("=" * 40)
-print("Prime League Teams in Alpha: {}".format(sorted(myPrimLeagTuple)))
-print("=" * 40)
-print("Manchester City Porsition is: {}".format(int(myPrimLeagTuple.index("Man City")+1)))
-print("=" * 40)
+a = 0
+option = input("Please Choose \n [0] For Top Ranked Teams \n [1] For Last Ranked Teams \n")
+if option not in ("0","1"):
+    print("Invalid Option")
+else:
+    numberOfTeams = input("please enter how many Teams you want to display: ")
+    if option == "0":
+        while a < int(numberOfTeams):
+            print(f"{a + 1} - {myPrimLeagTuple[a]}")
+            a += 1
+    else:
+        lastRankedTeams = myPrimLeagTuple[-int(numberOfTeams):]
+        while a < len(lastRankedTeams):
+            print(f"{a + 1} - {lastRankedTeams[a]}")
+            a += 1
---
+++ TheoryAndExamples/loop_while.py
@@ -0,0 +1,15 @@
+#############################################
+#           ##### While Loop #####          #
+#############################################
+# while <condition>: # will work as condtion is true till condition become false
+#   do something
+# [else:] #optional
+#   do something
+
+a = 0
+while a < 10:
+    print(a)
+
+    a +=1
+else:
+    print("pass")
